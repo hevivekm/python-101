a59e41e495b799356b04bd1085e11fb34b3be10c
Vivek <13690485+hevivekm@users.noreply.github.com>
2019-10-25 12:11:39 +0530

    Adding variables

---
+++ variables.py
@@ -0,0 +1,7 @@
+failed_subjects = "2"  # assign
+name = 'John'  # assign
+print('Dear Mrs Badger')
+print('Your son ' + name + ' is failing ' + failed_subjects + ' subjects.')
+print(name + ' will need to redo ' + failed_subjects + '  courses.')
+name = "Eric"  # reassign
+print(name + ' is doing well in geography.')
